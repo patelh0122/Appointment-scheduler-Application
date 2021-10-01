7a4ddfb2525cc519ca219696579f2fac3e1a7b57
Harsh <85594444+patelh0122@users.noreply.github.com>
2021-10-01 16:34:53 -0500

    Add files via upload
    Question 1

---
+++ Question1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace test4
+{
+    class Question1
+    {
+        static void Round(ref int a, ref int b)
+        {
+            int remainder = b % 15;
+            b -= remainder;
+            if (remainder > (15 / 2))
+                b += 15;
+            if(b == 60)
+            {
+                b = 0;
+                a += 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the hour:");
+            int a = int.Parse(Console.ReadLine());
+            while(!(a >= 0 && a < 24))
+            {
+                Console.WriteLine("Enter a valid hour between range 0 and 23.");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the minute");
+            int b = int.Parse(Console.ReadLine());
+            while(!(b >= 0 && b < 60))
+            {
+                Console.WriteLine("Enter a valid Minute between 0 and 59.");
+                b = int.Parse(Console.ReadLine());
+            }
+            Round(ref a, ref b);
+            Console.Write("The time is ");
+            Console.Write(a);
+            Console.Write(":");
+            if (b < 10)
+                Console.Write(0);
+            Console.Write(b);
+
+        }
+    }
+}
